e4b8653194a8c6bbf681a448aa8010f0b9188ce8
MarziaYarzada <marzaiyarzada2@gmail.com>
2025-06-02 08:28:33 +0430

    create core Express server configuration

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
-DATABASE_URL="postgresql://username:password@localhost:5432/warehouse_db"
-JWT_SECRET="your_jwt_secret"
+import express from 'express'
+import cors from 'cors';
+import authRoutes from './routes/auth/auth.routes';
+
+ const app=express();
+ app.use(cors());
+ app.use(express.json())
+ app.get('/',(req,res)=>{
+    res.send(' Warehouse Inventory API is running...')
+ })
+ app.use('/api/auth', authRoutes);
+
+ export default app;
