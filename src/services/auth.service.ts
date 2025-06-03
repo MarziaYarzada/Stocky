b23f01571f4afd32356c42e89db9101b36cebfbe
MarziaYarzada <marziayarzada2@gmail.com>
2025-06-03 19:23:35 +0430

    implement auth service with JWT

---
+++ src/services/auth.service.ts
@@ -0,0 +1,24 @@
+import prisma from "../prisma/client";
+import {
+  hashPassword,
+  comparePassword,
+  generateToken,
+} from "../utils/auth.util";
+
+export const registerUser = async (username: string, password: string) => {
+  const existing = await prisma.user.findUnique({ where: { username } });
+  if (existing) throw new Error("Username is already taken");
+  const hashed = await hashPassword(password);
+  const user = await prisma.user.create({
+    data: { username, password: hashed, role: "STAFF" },
+  });
+  const token = generateToken(user.id, user.role);
+  return { user, token };
+};
+export const loginUser = async (username: string, password: string) => {
+  const user = await prisma.user.findUnique({ where: { username } });
+  if (!user || !(await comparePassword(password, user.password)))
+    throw new Error("Invalid credentials");
+  const token = generateToken(user.id, user.role);
+  return { user, token };
+};
